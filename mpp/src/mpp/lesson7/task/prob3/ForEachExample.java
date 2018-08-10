4d254dc15d403d3ca7ab5bade704020acfd79fdd
986505 <986505@CS_LAB218-202.cs.mum.edu>
2018-08-10 14:37:15 -0500

    changes

--- mpp/src/mpp/lesson7/task/prob3/ForEachExample.java
+++ mpp/src/mpp/lesson7/task/prob3/ForEachExample.java
@@ -9,18 +9,27 @@ import java.util.function.Consumer;
 public class ForEachExample {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", 
-				"Away", "On Vacation", "Everywhere you want to be");
+		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon","Away", "On Vacation", "Everywhere you want to be");
 		Consumer<String> doSomething = (String s) -> System.out.println(s.toUpperCase());
 		list.forEach(doSomething);
 		//print each element of the list in upper case format
 		
 		
+		List<Integer> listInts =Arrays.asList(2,2,34,5,6,8);
+		Consumer<Integer> doIntegersonethimg = (Integer i) -> System.out.println(i);
+		listInts.forEach(doIntegersonethimg);
 		
+		Person p1 = new Person("carlos");
+		Person p2 = new Person("pedro");
+		List<Person> lisperson = Arrays.asList(p1,p2);
+		Consumer<Person> doperson = p -> System.out.println(p.getName());
+		lisperson.forEach(doperson);
 		
 	}
+	
 	
 	//implement a Consumer
 	
 	
 }
+
---
+++ mpp/src/mpp/lesson7/task/prob3/Person.java
@@ -0,0 +1,14 @@
+package mpp.lesson7.task.prob3;
+
+public class Person {
+	private String name ;
+	public Person (String pname) {
+		name=pname;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
