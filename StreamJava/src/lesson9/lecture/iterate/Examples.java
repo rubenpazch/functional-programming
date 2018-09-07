707925d8f4e5ec95db8e32e19f76e332028a6930
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2018-09-06 19:08:08 -0500

    prueba

---
+++ StreamJava/src/lesson9/lecture/iterate/Examples.java
@@ -0,0 +1,25 @@
+package democode.lesson9.lecture.iterate;
+import java.math.BigInteger;
+import java.util.stream.*;
+public class Examples {
+
+	public static void main(String[] args) {
+		Stream<BigInteger> stream1 = 
+			Stream.iterate(BigInteger.ONE, n -> n.add(BigInteger.ONE));
+		
+		Stream<Integer> stream2 =
+			Stream.iterate(1, n -> n + 1);
+		
+		//stream1.limit(5).collect(Collectors.toList()).forEach(System.out::println);
+		//stream2.limit(5).collect(Collectors.toList()).forEach(System.out::println);
+		
+		Stream<Integer> odd =
+				Stream.iterate(1, n -> n+1).filter(n->n%2!=0);
+
+		odd.limit(8).skip(4).collect(Collectors.toList()).forEach(System.out::println);	
+		//System.out.println( odd.limit(10).skip(4).collect(Collectors.toList()));
+		
+		
+	}
+
+}
